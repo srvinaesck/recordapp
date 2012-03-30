7a8ab3eef23f53b55c36cd72240ebe2fcab672c6
Alejandro Servin <alejandroservin90210@gmail.com>
2012-03-30 16:07:53 -0500

    Retomando tesis: Marzo 30, 2012

---
+++ src/recordapp/alfa/RecordAppActivity.java
@@ -0,0 +1,13 @@
+package recordapp.alfa;
+
+import android.app.Activity;
+import android.os.Bundle;
+/** Prototipo creado Marzo 22 2012 */
+public class RecordAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
